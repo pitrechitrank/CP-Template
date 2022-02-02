7c12bf7347f99cb5e8880635a69146d1f1c9b72b
Chitrank Pitre <thechitrank@gmail.com>
2022-02-02 21:51:35 +0530

    Competitive Programming C++ Template

---
+++ solution.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  #endif 
+  /* Start here*/
+  
+
+  return 0;
+}
